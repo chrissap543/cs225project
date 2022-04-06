6ea8477282f70418f730777f0f574792e097b23d
shrijan2 <shrijan2@illinois.edu>
2022-04-06 01:56:17 +0000

    graph pseudocode

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,58 @@
 #include <iostream>
 
+
 int main() {
+
+Graph graph = new Graph();
+buildgraph(graph);
+
+
+
   
 }
+
+Graph buildgraph(Graph graph){
+    string source;
+    string target;
+
+    Node* sourcenode;
+    Node* targetnode;
+
+    std::map<string, Node *> mp;
+
+
+    while(file has next line){
+        source = file col 1;
+        target = file col 2;
+
+        if (mp.find(source) != mp.end() && mp.find(target) == map.end()){
+            auto it = mp.find(source);
+            targetnode.name() = target;
+            mp.insert({target, targetnode});
+            it->second->addNeighbor(targetnode);
+        }
+        
+        else if(mp.find(source) != mp.end() && mp.find(target) != map.end()){
+
+        }
+        else if(mp.find(source) == mp.end() && mp.find(target) != map.end()){
+            auto it = mp.find(target);
+            sourcenode.name() = source;
+            mp.insert({source, sourcenode});
+            targetnode.addNeighbor(it->second);
+        }
+        else{
+            sourcenode.name() = source;
+            targetnode.name() = target;
+            mp.insert({source, sourcenode});
+            mp.insert({target, targetnode});
+            sourcenode.addNeighbor(targetnode);
+        }
+
+    }
+
+    
+
+}
+
+
