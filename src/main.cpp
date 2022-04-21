0fe901c527889c71c9a5e16aea1d648ad6f53859
cs79 <cs79@illinois.edu>
2022-04-21 06:59:18 +0000

    fixing merge conflicts

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,7 @@
 #include "processdata.hpp"
 #include <iostream>
-// #include <matrix.hpp>
-
 
 int main() {
-
   Graph graph = buildGraph("data/fixed_data.csv"); 
   std::unordered_map<std::string, Node*> mp = graph.getNodes(); 
   std::cout << mp.size() << std::endl; 
