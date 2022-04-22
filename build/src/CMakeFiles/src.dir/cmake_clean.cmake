e0c38941a1f18a7a0b2c46b7b85b07cc1eef1000
cs79 <cs79@illinois.edu>
2022-04-22 22:49:42 +0000

    worked on CMake, include directory broken

---
+++ build/src/CMakeFiles/src.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/src.dir/graph.cpp.o"
+  "CMakeFiles/src.dir/main.cpp.o"
+  "CMakeFiles/src.dir/matrix.cpp.o"
+  "CMakeFiles/src.dir/node.cpp.o"
+  "CMakeFiles/src.dir/processdata.cpp.o"
+  "libsrc.a"
+  "libsrc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
